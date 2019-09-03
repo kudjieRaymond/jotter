4857600f864fe25c43db5c1a9f8102913220a5d3
Ray Kudjie <rkudjie@gmail.com>
2019-09-03 23:32:07 +0000

    Front end Api Service

---
+++ vue-task/common/client.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+import JwtService from './jwt-service'
+import { API_URL } from './config.js'
+
+const client = axios.create({
+	baseURL : API_URL
+})
+
+client.defaults.headers.common['Authorization'] = `Bearer ${JwtService.getToken()}`;
+client.defaults.headers.common['Content-type'] = 'application/json' ;
+client.defaults.headers.common['Accept'] = 'application/json' ;
+
+export default {
+	all(params){
+		return client.get('tasks', params)
+	},
+	find(id){
+		return client.get(`tasks/${id}`)
+
+	},
+	create(url, data){
+		return client.post(url, data)
+	},
+	update(id, data){
+		return client.put(`tasks/${id}`, data)
+	},
+	delete(id){
+		return client.delete(`tasks/${id}`)
+	}
+}
---
+++ vue-task/common/config.js
@@ -0,0 +1,3 @@
+const API_URL = 'htpp://192.168.10.10/api/v1'
+
+export default API_URL
